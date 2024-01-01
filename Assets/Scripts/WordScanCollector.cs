51db63c37e88e3d865e58150a090927e11b47c25
JoJo777890 <JoJo777890@gmail.com>
2024-01-02 03:20:04 +0800

    Correct Words Add Scores (Debug Message)
    2024-1-1

--- Assets/Scripts/WordScanCollector.cs
+++ Assets/Scripts/WordScanCollector.cs
@@ -7,7 +7,10 @@ public class WordScanCollector : MonoBehaviour
 {
     public Text scanMessage;
     public GameObject[] bookSymbolObjects;
-    public string fullMessage;
+    public string[] animalNames = { "cat", "fox", "bear", "wolf", "tiger", "eagle", "rhino" };
+
+    private string currMessage;
+    private string fullMessage;
     //public Scan[]
     //public List<Scan> scan = new List<Scan>();
 
@@ -26,7 +29,22 @@ public class WordScanCollector : MonoBehaviour
         // 將當下bookSymbolObjects[]陣列內容，一個一個加到要顯示的 String
         for (int i = 0; i < bookSymbolObjects.Length; i++)
         {
-            fullMessage += bookSymbolObjects[i].GetComponent<Scan>().displayText;
+            // currMessage 取得在 bookSymbolObjects 陣列中，Scan到的字串。
+            currMessage = bookSymbolObjects[i].GetComponent<Scan>().displayText;
+
+            // 顯示訊息 + currMessage
+            fullMessage += currMessage;
+
+            // 如果 currMessage 符合陣列中的動物名稱，在訊息後面加上"Correct! (+100)"
+            for (int j = 0; j < animalNames.Length; j++)
+            {
+                if (string.Equals(currMessage, animalNames[j]) == true)
+                {
+                    fullMessage += " Correct! (+100)";
+                }
+            }
+
+            // 顯示訊息 + "下一行"
             fullMessage += "\n";
         }
 
